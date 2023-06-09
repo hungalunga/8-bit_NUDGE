60ddda37d4da58886d9fcf0ac2969ba9d95e71e1
Hung <hungalunga.vo@gmail.com>
2023-06-09 10:16:26 +0100

    adding divs to app

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -1,21 +1,24 @@
 
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header">NUDGE
+      <div className="Profile">
+        <h2>Welcome Back</h2>
+        <h3>UserName</h3>
+      </div>
+      <div className="Quiz">
+        <button className="Quiz-button">Quiz</button>
+      </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Learn React
-        </a>
       </header>
     </div>
   );
