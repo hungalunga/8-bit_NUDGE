f81700f19ce50ea26a179c6691c49d659967fcea
SimplyEly <elydreaves@hotmail.co.uk>
2023-06-12 12:28:29 +0100

    question functionality beginning

--- my-app/src/Components/MainQuiz/MainQuiz.js
+++ my-app/src/Components/MainQuiz/MainQuiz.js
@@ -1,7 +1,44 @@
-export default function MainQuiz() { 
-    return (
-        <div>
-            
-        </div>
-    )
+import QuestionDisplay from "../QuestionDisplay/QuestionDisplay";
+
+export default function MainQuiz() {
+  const quizQuestions = [
+    {
+      question: "59 + 95",
+      answer: 154,
+      wrong_answers: [54, 102, 112],
+    },
+    {
+      question: "313 x 5",
+      level: "HARD",
+      answer: 1565,
+      wrong_answers: [1315, 1535, 1554],
+    },
+    {
+      question: "376 - 1",
+      answer: 375,
+      wrong_answers: [275, 323, 333],
+    },
+    {
+      question: "304 x 79",
+      answer: 24016,
+      wrong_answers: [23766, 23986, 24005],
+    },
+    {
+      question: "116 - 79",
+      answer: 37,
+      wrong_answers: [36, 32, -13],
+    },
+  ];
+
+  function randomQuestion(questionArray) {
+    return questionArray[Math.floor(Math.random() * questionArray.length)];
+  }
+
+  var questionObject = randomQuestion(quizQuestions);
+
+  return (
+    <div>
+      <QuestionDisplay questionObject={questionObject} />
+    </div>
+  );
 }
--- my-app/src/Components/QuestionDisplay/QuestionDisplay.js
+++ my-app/src/Components/QuestionDisplay/QuestionDisplay.js
@@ -1,7 +1,27 @@
-export default function QuestionDisplay() { 
+
+    // retrieve the question and answer data from the "data base" --done
+        // assign the question to a state variable
+        // call variable in the return
+
+// display the question on the page
+
+// import answer component
+
+// call answer component in the return
+
+
+
+
+import AnswerCheckbox from "../AnswerCheckbox/AnswerCheckbox"
+
+
+export default function QuestionDisplay(props) {
+
+
+
     return (
         <div>
-            
+            <AnswerCheckbox/>
         </div>
     )
 }
