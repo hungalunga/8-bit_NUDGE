03cf98062b553b2b93b3e7dc2c9e9784d118f188
Hung <hungalunga.vo@gmail.com>
2023-06-12 11:58:11 +0100

    data in main quiz

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -1,18 +1,8 @@
-import react, { useState } from 'react';
+
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const [questions, setQuestions] = useState([]);
-  let database = {
-		"question": "59 + 95",
-		"level": "MEDIUM",
-		"answer": 154,
-		"wrong_answers": [54, 102, 112]
-	}
-
-  setQuestions(database);
 
 
   return (
@@ -22,21 +12,7 @@ function App() {
       <div className="Profile">
       Welcome back
       <div className='UserName'>Username</div>
-      
       </div>
-      <div>
-      {questions.map((question, index) => (
-        <div key={index}>
-          <h3>{question.question}</h3>
-          <ul>
-            {question.wrong_answers.map((answer, optionIndex) => (
-              <li key={optionIndex}>{answer}</li>
-            ))}
-            <li>{question.answer}</li>
-          </ul>
-        </div>
-      ))}
-    </div>
       <div className="Quiz">
       <iframe src="https://quizlet.com/516241505/match/embed?i=5647tq&x=1jj1" height="500" width="100%"></iframe>
         <button className="Quiz-button">Quiz</button>
