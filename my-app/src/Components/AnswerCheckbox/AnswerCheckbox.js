9e11ee7343dcd673649e119a803fabc161a28529
Hung <hungalunga.vo@gmail.com>
2023-06-13 09:20:23 +0100

    checkbox looking like its done so far

--- my-app/src/Components/AnswerCheckbox/AnswerCheckbox.js
+++ my-app/src/Components/AnswerCheckbox/AnswerCheckbox.js
@@ -5,20 +5,60 @@ we want to display the answer in a checkbox format
 we want to be able to select only one answer at a time, when the user selects a new answer, the previous answer should be unselected
 */
 
+/* Checkbox functions
+each checkbox needs to be randomly placed on the page
+each checkbox needs to be assigned a value
+*/
 import React from 'react';
+import { useState,useEffect } from 'react';
 import { Checkbox } from 'primereact/checkbox';
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+
+const shuffleArray = (array) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+    return shuffledArray;
+  };
 
 export default function AnswerCheckbox(props) {
-    const { quizQuestions } = props;
+    const { quizAnswers } = props;
 
+  const [answer, setAnswer] = useState("");
+  const [allAnswers, setAllAnswers] = useState([]);
+
+  useEffect(() => {
+    const shuffledAnswers = shuffleArray([quizAnswers.answer, ...quizAnswers.wrong_answers]);
+    setAllAnswers(shuffledAnswers);
+  }, []);
+
+  const onAnswerChange = (e) => {
+    setAnswer(e.value);
+    console.log(answer);
+  };
+  
     return (
-        <div>
-            <h1>AnswerCheckbox</h1>
-            <Checkbox value={quizQuestions[0].answer}/>
-            <label>{quizQuestions[0].answer}</label>
-
+        <div className="card flex flex-wrap justify-content-center gap-3">
+          <div className="flex align-items-center">
+            <div className="answerBox">
+            <h3>{answer}</h3>
+              {allAnswers.map((answerOption, index) => (
+                <div className = "checkbox" key={index}>
+                  <Checkbox
+                    inputId={`answer${index + 1}`}
+                    value={answerOption}
+                    onChange={onAnswerChange}
+                    checked={answer === answerOption}
+                  />
+                  <label>{answerOption}</label>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-    )
 
-}
-
+      );
+    };
