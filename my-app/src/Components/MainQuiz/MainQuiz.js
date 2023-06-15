ac24fc3051efea17e0a3110c2320d69c6d99b03a
CitizenBabbage <molyneux73@protonmail.com>
2023-06-15 16:13:28 +0100

    added a simple working test to MainQuiz.test.js

--- my-app/src/Components/MainQuiz/MainQuiz.js
+++ my-app/src/Components/MainQuiz/MainQuiz.js
@@ -136,7 +136,7 @@ useEffect(() => {
 
 
   return (
-    <div>
+    <div data-testid="question-display">
       <QuestionDisplay questionObject={questionObject} questionNumber= {questionNumber} setQuestionNumber = {setQuestionNumber} incorrectAnswers = {incorrectAnswers} setIncorrectAnswers = {setIncorrectAnswers}/>
     </div>
   );
