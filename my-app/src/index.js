f0c5c8d740239ab78ca973cef0cf4b3d8370fbe1
SimplyEly <elydreaves@hotmail.co.uk>
2023-06-19 16:26:52 +0100

    started styling login page

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './Components/Login/Login';
+import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
@@ -9,8 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Login />
-
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
