9d0f3c2e3deda9bfa50e845e37ce019faa910ab4
Rosie Jones <rosiegracejones@gmail.com>
2023-06-21 14:07:56 +0100

    have begun to add flex layout to dashboard

--- my-app/src/Components/Dashboard/Dashboard.js
+++ my-app/src/Components/Dashboard/Dashboard.js
@@ -15,15 +15,13 @@ export default function Dashboard() {
 				<div>icon</div>
 			</div>
 			<div className="dashboard-page">
-				<div className="welcome-container">
-					<Avatar
-						label="P"
-						size="xlarge"
-						shape="circle"
-						className="circleAvatar"
-					/>
-					<p>Welcome Back</p>
-					<h1>ashwantspizza</h1>
+				<div className="dashboard-top">
+					<div className="welcome-container">
+						<Avatar label="A" size="xlarge" className="circleAvatar" />
+						<div className="welcome-text">
+							<p>Welcome Back</p>
+							<h1>ashwantspizza</h1>
+						</div>
 					</div>
 					<div className="user-scores">
 						<Skeleton
@@ -50,14 +48,14 @@ export default function Dashboard() {
 				<Divider />
 				<div className="dashboard-bottom">
 					<div className="learning-container">
-						<h2>today's learning quiz</h2>
+						<h2>Your Learning</h2>
 						<Link to="/quiz">
 							<Button label="Today's Quiz" size="large" />
 						</Link>
 					</div>
 
 					<div className="leaderboard-container">
-						<h2>leaderboard</h2>
+						<h2>Leaderboard</h2>
 						<DataTable tableStyle={{ minWidth: "50rem" }}>
 							<Column field="user" header="User"></Column>
 							<Column field="ranking" header="Ranking"></Column>
@@ -65,6 +63,7 @@ export default function Dashboard() {
 						</DataTable>
 					</div>
 				</div>
+			</div>
 		</>
 	);
 }
