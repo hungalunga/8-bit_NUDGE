d869e89ef7681779273e8b4a6445d3d00634c2c0
ashbsmith <aislingbsmith@gmail.com>
2023-06-27 16:45:59 +0100

    texttospeech pushed to local

--- my-app/src/Components/PromptQuizDisplay/PromptQuizDisplay.js
+++ my-app/src/Components/PromptQuizDisplay/PromptQuizDisplay.js
@@ -5,12 +5,8 @@ import AnswerCheckbox from "../AnswerCheckbox/AnswerCheckbox";
 import PromptQuestionTimer from "../PromptQuestionTimer/PromptQuestionTimer";
 import PromptQuizCorrect from "../PromptQuizCorrect/PromptQuizCorrect";
 import PromptQuizIncorrect from "../PromptQuizIncorrect/PromptQuizIncorrect";
-import { ProgressBar } from "primereact/progressbar";
-import { Card } from "primereact/card";
-import { Button } from "primereact/button";
 import TextToSpeech from "../TextToSpeech/TextToSpeech";
 import "../PromptQuiz/PromptQuiz.css";
-
 
 export default function PromptQuizDisplay(props) {
 	const [withinTime, setWithinTime] = useState(true);
