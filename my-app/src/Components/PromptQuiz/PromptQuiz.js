b77949fd6b419917d4c710994b3e38f8f1776bdb
Hung <hungalunga.vo@gmail.com>
2023-06-27 11:10:07 +0100

    g

--- my-app/src/Components/PromptQuiz/PromptQuiz.js
+++ my-app/src/Components/PromptQuiz/PromptQuiz.js
@@ -30,6 +30,7 @@ export default function PromptQuiz(props) {
 			getOneQuestion(data);
 		}
 		getQuestions();
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [questionNumber]);
 
 	function getOneQuestion(data) {
